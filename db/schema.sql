02f530df7dcd0018178c1ab7ffbaf9560224b224
aenz <andrewjensor@gmail.com>
2022-12-07 22:21:24 -0500

    fixing some problems in schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -21,8 +21,12 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER, 
-    INDEX role_ind (role_id),
+    INDEX role_id (role_id),
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     manager_id INTEGER,
     INDEX manager_ind (manager_id),
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+
+
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,19 @@
 INSERT INTO department (name)
 VALUES 
-
 ('Food Service'),
 ('Advertising'),
-('Cleaning')
+('Cleaning');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-
 ('Cashier', 22, 1),
 ('Janitor', 20, 3),
 ('Burger-Flipper', 15, 1),
-('Sign-Twirler', 11, 2),
-
-
+('Sign-Twirler', 11, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-
 ("Jimbo", "McGee", 4, 3),
 ("Brad", "Pitt", 3, null ),
 ("Selena", "Gomez", 1, null),
-("Isaac", "Newton", 2, 3)
+("Isaac", "Newton", 2, 3);
