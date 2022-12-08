2eb1a4fec1a44542ad02578cc0d20c8ebcc3a00b
aenz <andrewjensor@gmail.com>
2022-12-07 20:27:17 -0500

    connection

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ const cTable = require('console.table');
 
 require('dotenv').config()
 
+const connection = mysql.startConnect({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'employee_db'
+  });
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+  afterConnection();
+
+
+})
